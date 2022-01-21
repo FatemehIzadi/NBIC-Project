3f060b5a08d6dee8260c8046849bc90ead66ae69
FatemehIzadi <44497643+FatemehIzadi@users.noreply.github.com>
2022-01-22 01:08:59 +0330

    fixed errors in insructions

--- src/Instructions.js
+++ src/Instructions.js
@@ -2,48 +2,6 @@ import React from "react";
 import {Card, Button, InputGroup, FormControl, Form} from 'react-bootstrap';
 import InstructionsCard from "./InstructionsCard";
 
-
-const renderCheckbox = ({ input, label }) =>
-  <FormControlLabel
-    control={
-      <Checkbox
-        {...input}
-        checked={input.value ? true : false}
-        onCheck={input.onChange}
-      />
-    }
-    label={label}
-  />
-
-const CreateReport = (props) => {
-  const { handleSubmit, pristine, submitting } = props;
-
-  return (
-    <div className="create-report-form-container mobile-padding">
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Field name="quotation-checkbox" component={renderCheckbox} label="Quotation to do" />
-        </div>
-
-        <div>
-          <Button raised color="accent" label="CreateQuotation">
-            Create a Quotation
-          </Button>
-          <Button raised color="accent" label="TakeNotes">
-            Take some notes
-          </Button>
-        </div>
-
-
-        <Button raised color="accent" type="submit" label="send" disabled={pristine || submitting}>
-          Send
-        </Button>
-
-      </form>
-    </div>
-  );
-};
-
 export default function Instructions() {
 
 
